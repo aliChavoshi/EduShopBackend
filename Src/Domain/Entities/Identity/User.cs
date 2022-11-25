8b01c8159519f9a3e7645d2c02a547773034060f
Ali Chavoshi <alichavoshii1372@gmail.com>
2022-11-25 11:35:44 +0330

    5- Add Register user

--- Src/Domain/Entities/Identity/User.cs
+++ Src/Domain/Entities/Identity/User.cs
@@ -5,6 +5,7 @@ namespace Domain.Entities.Identity;
 public class User : IdentityUser
 {
     public string DisplayName { get; set; }
+    public string NationalCode { get; set; }
 
     public List<Address> Addresses { get; set; }
     public ICollection<UserRole> UserRole { get; set; }
